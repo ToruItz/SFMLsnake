d5107d887e2620fb69bad42797f6315f5b20b357
Pavel <toruchan174@gmail.com>
2022-06-15 19:14:46 +0300

    Build 1.5 Build notes: -Release multiplayer snake [Beta] -Fixed multiplayer apple -Removed dynamic window scale -Fixed Collision with apple -Fixed connections failures in multiplayer mode

--- main.cpp
+++ main.cpp
@@ -9,14 +9,8 @@
 #include <ws2tcpip.h>
 #include <windows.h>
 
-#pragma comment(lib, "sfml-system")
-#pragma comment(lib, "sfml-window")
-#pragma comment(lib, "sfml-graphics")
-#pragma comment(lib, "sfml-audio")
-
-#pragma comment(lib, "user32.lib")
-int X = GetSystemMetrics(SM_CXSCREEN);
-int Y = GetSystemMetrics(SM_CYSCREEN);
+#define X 2560
+#define Y 1440
 #define START_POINTS 50
 #define LOCALHOST "25.73.9.134"
 #define PORT 8000
@@ -75,9 +69,9 @@ void drawSnake(std::vector<CircleShape> player, RenderWindow* window){
 }
 
 int main() {
-    int choose;
+    int mode;
 
-    std::cout << "1 or 2?"; std::cin >> choose;
+    std::cout << "1 or 2?"; std::cin >> mode;
 
     WSADATA WSA;
     WSAStartup(WINSOCK_VERSION, &WSA);
@@ -88,29 +82,21 @@ int main() {
     addr.sin_family = AF_INET;
 
     SOCKET inoutstream = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-    bind(inoutstream, (SOCKADDR*)&addr, sizeof(addr));
 
     sockaddr_in client;
     int sizeOfClient = sizeof(client);
-
-    if(choose == 1){
-        recvfrom(inoutstream, 0, 0, 0, (sockaddr*)&client, &sizeOfClient);
-    }else{
-        sendto(inoutstream, 0, 0, 0, (sockaddr*)&addr, sizeof(addr));
-    }
-
 
     srand(time(NULL));
     std::mt19937 random(rand());
     RenderWindow window(VideoMode(X, Y), "Suck!", Style::Fullscreen);
 
-    std::vector<CircleShape> player1(START_POINTS);
-    player1[0].setPosition(X/2 - 50, Y/2);
-    player1 = init(player1);
+    std::vector<CircleShape> snakefirst(START_POINTS);
+    snakefirst[0].setPosition(X / 2 - 50, Y / 2);
+    snakefirst = init(snakefirst);
 
-    std::vector<CircleShape> player2(START_POINTS);
-    player2[0].setPosition(X/2 + 50, Y/2);
-    player2 = init(player2);
+    std::vector<CircleShape> snakesecond(START_POINTS);
+    snakesecond[0].setPosition(X / 2 + 50, Y / 2);
+    snakesecond = init(snakesecond);
 
     CircleShape apple;
     apple.setFillColor(Color::Red);
@@ -152,7 +138,7 @@ int main() {
 
     Text Build;
     Build.setFillColor(Color::White);
-    Build.setString("Build 1.0");
+    Build.setString("Build 1.3");
     Build.setCharacterSize(20);
     Build.setPosition(X - 20, Y - 30);
     Build.setFont(font);
@@ -167,6 +153,21 @@ int main() {
     window.setFramerateLimit(30);
     char derection1 = 'u';
     char derection2 = 'u';
+
+    Vector2f buff = apple.getPosition();
+    bool needBuff = false;
+
+    if(mode == 1){
+        bind(inoutstream, (SOCKADDR*)&addr, sizeof(addr));
+        recvfrom(inoutstream, 0, 0, 0, (sockaddr*)&client, &sizeOfClient);
+        std::cout << "Connected!" << std::endl;
+        recvfrom(inoutstream, (char*)&buff, sizeof(buff), 0, (sockaddr*)&client, &sizeOfClient);
+        std::cout << "2Connected!" << std::endl;
+    }else{
+        sendto(inoutstream, 0, 0, 0, (sockaddr*)&addr, sizeof(addr));
+        sendto(inoutstream, (char*)&buff, sizeof(buff), 0, (sockaddr*)&addr, sizeof(addr));
+    }
+
     while(window.isOpen()){
         Time time = clock.getElapsedTime();
         framePerSecond.setString("FPS: " + std::to_string(1.f / time.asSeconds()));
@@ -182,44 +183,59 @@ int main() {
         else if(Keyboard::isKeyPressed(Keyboard::Down) && derection1 != 'u') derection1 = 'd';
         else if(Keyboard::isKeyPressed(Keyboard::Left) && derection1 != 'r') derection1 = 'l';
 
-        if(choose == 1){
-            sendto(inoutstream, (char*)&apple, sizeof(apple), 0, (sockaddr*)&client, sizeof(client));
+        if(mode == 1){
             sendto(inoutstream, (char*)&derection1, sizeof(derection1), 0, (sockaddr*)&client, sizeof(client));
             recvfrom(inoutstream, (char*)&derection2, sizeof(derection2), 0, (sockaddr*)&client, &sizeOfClient);
+            Vector2f pos = apple.getPosition();
+            if(buff != pos) {
+                needBuff = true;
+                sendto(inoutstream, (char*)&needBuff, sizeof(needBuff), 0, (sockaddr*)&client, sizeof(client));
+                sendto(inoutstream, (char *) &pos, sizeof(pos), 0, (sockaddr *) &client, sizeof(client));
+                needBuff = false;
+            }else{
+                sendto(inoutstream, (char*)&needBuff, sizeof(needBuff), 0, (sockaddr*)&client, sizeof(client));
+            }
         }else{
-            recvfrom(inoutstream, (char*)&apple, sizeof(apple), 0, (sockaddr*)&addr, &sizeofaddr);
             recvfrom(inoutstream, (char*)&derection2, sizeof(derection2), 0, (sockaddr*)&addr, &sizeofaddr);
             sendto(inoutstream, (char*)&derection1, sizeof(derection1), 0, (sockaddr*)&addr, sizeof(addr));
+            recvfrom(inoutstream, (char*)&needBuff, sizeof(needBuff), 0, (sockaddr*)&addr, &sizeofaddr);
+            if(needBuff) {
+                Vector2f pos;
+                recvfrom(inoutstream, (char *) &pos, sizeof(pos), 0, (sockaddr *) &addr, &sizeofaddr);
+                apple.setPosition(pos);
+            }
         }
 
-        if(derection1 == 'r' && player1[0].getPosition().x > X) player1[0].setPosition(0, player1[0].getPosition().y);
-        else if(derection1 == 'l' && player1[0].getPosition().x < 0) player1[0].setPosition(X, player1[0].getPosition().y);
-        else if(derection1 == 'u' && player1[0].getPosition().y < 0) player1[0].setPosition(player1[0].getPosition().x, Y);
-        else if(derection1 == 'd' && player1[0].getPosition().y > Y) player1[0].setPosition(player1[0].getPosition().x, 0);
+        if(derection1 == 'r' && snakefirst[0].getPosition().x > X) snakefirst[0].setPosition(0, snakefirst[0].getPosition().y);
+        else if(derection1 == 'l' && snakefirst[0].getPosition().x < 0) snakefirst[0].setPosition(X, snakefirst[0].getPosition().y);
+        else if(derection1 == 'u' && snakefirst[0].getPosition().y < 0) snakefirst[0].setPosition(snakefirst[0].getPosition().x, Y);
+        else if(derection1 == 'd' && snakefirst[0].getPosition().y > Y) snakefirst[0].setPosition(snakefirst[0].getPosition().x, 0);
 
-        if(derection2 == 'r' && player2[0].getPosition().x > X) player2[0].setPosition(0, player2[0].getPosition().y);
-        else if(derection2 == 'l' && player2[0].getPosition().x < 0) player2[0].setPosition(X, player2[0].getPosition().y);
-        else if(derection2 == 'u' && player2[0].getPosition().y < 0) player2[0].setPosition(player2[0].getPosition().x, Y);
-        else if(derection2 == 'd' && player2[0].getPosition().y > Y) player2[0].setPosition(player2[0].getPosition().x, 0);
+        if(derection2 == 'r' && snakesecond[0].getPosition().x > X) snakesecond[0].setPosition(0, snakesecond[0].getPosition().y);
+        else if(derection2 == 'l' && snakesecond[0].getPosition().x < 0) snakesecond[0].setPosition(X, snakesecond[0].getPosition().y);
+        else if(derection2 == 'u' && snakesecond[0].getPosition().y < 0) snakesecond[0].setPosition(snakesecond[0].getPosition().x, Y);
+        else if(derection2 == 'd' && snakesecond[0].getPosition().y > Y) snakesecond[0].setPosition(snakesecond[0].getPosition().x, 0);
 
-        player1 = moveSnake(player1, 20, derection1);
-        player2 = moveSnake(player2, 20, derection2);
-
-        if(apple.getGlobalBounds().intersects(player1[0].getGlobalBounds())){
+        snakefirst = moveSnake(snakefirst, 20, derection1);
+        snakesecond = moveSnake(snakesecond, 20, derection2);
+        Vector2f snPos = snakefirst[0].getPosition();
+        Vector2f snsPos = snakesecond[0].getPosition();
+        Vector2f apPos = apple.getPosition();
+        if(abs(snPos.x - apPos.x) <= 100 && abs(snPos.y - apPos.y) <= 100){
             apple.setPosition(random()%X,random()%Y);
             Apple.setPosition(apple.getPosition().x - 10, apple.getPosition().y - 30);
-            player1.push_back(player1[player1.size() - 1]);
-            player1 = initColor(player1);
+            snakefirst.push_back(snakefirst[snakefirst.size() - 1]);
+            snakefirst = initColor(snakefirst);
             score1++;
             sound.setBuffer(buffer);
             sound.play();
         }
 
-        if(apple.getGlobalBounds().intersects(player2[0].getGlobalBounds())){
+        if(abs(snsPos.x - apPos.x) <= 100 && abs(snsPos.y - apPos.y) <= 100){
             apple.setPosition(random()%X,random()%Y);
             Apple.setPosition(apple.getPosition().x - 10, apple.getPosition().y - 30);
-            player2.push_back(player2[player2.size() - 1]);
-            player2 = initColor(player2);
+            snakesecond.push_back(snakesecond[snakesecond.size() - 1]);
+            snakesecond = initColor(snakesecond);
             score2++;
             sound.setBuffer(buffer);
             sound.play();
@@ -231,18 +247,24 @@ int main() {
         window.clear();
 
         window.draw(framePerSecond);
-        drawSnake(player1, &window);
-        drawSnake(player2, &window);
+        drawSnake(snakefirst, &window);
+        drawSnake(snakesecond, &window);
         window.draw(apple);
         window.draw(Apple);
         window.draw(Score1);
         window.draw(Score2);
         window.draw(Build);
 
-        if(isColide(player1)) {
+        if(isColide(snakefirst)) {
             if(score1 > 5 && score1 - 5 != 0) {
                 score1 -= 5;
-                for (int i = 0; i < 5; ++i) player1.pop_back();
+                for (int i = 0; i < 5; ++i) snakefirst.pop_back();
+            }else window.close();
+        }
+        if(isColide(snakesecond)) {
+            if(score1 > 5 && score1 - 5 != 0) {
+                score1 -= 5;
+                for (int i = 0; i < 5; ++i) snakesecond.pop_back();
             }else window.close();
         }
 
